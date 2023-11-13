0a1dac038c433359f648b415e708c815b5140379
Reginald Kargbo <47126013+reginald441@users.noreply.github.com>
2023-11-12 23:18:01 -0500

    Add files via upload

---
+++ Reg M03 Programming Lists and Functions.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+thing=["mozzarella", "cinderella ", "salmonella"]
+print("Initial list of things")
+print(thing)
+thing[1]=thing[1].capitalize()
+print("List with person being capitalized")
+print(thing)
+thing[0]=thing[0].upper()
+print("List with cheesy thing converted to upper")
+print(thing)
+print("List with disease removed ")
+thing.remove(thing[2])
+print(thing)
+#add this if that is what is mend
+print("List with new element")
+thing.append("Noble Prize")
+print(thing)
+
+
+# In[2]:
+
+
+thing=["mozzarella", "cinderella ", "salmonella"]
+print("Initial list of things")
+print(thing)
+thing[1]=thing[1].capitalize()
+print("List with person being capitalized")
+print(thing)
+thing[0]=thing[0].upper()
+print("List with cheesy thing converted to upper")
+print(thing)
+print("List with disease removed ")
+thing.remove(thing[2])
+print(thing)
+#add this if that is what is mend
+print("List with new element")
+thing.append("Noble Prize")
+print(thing)
+
+
+# In[ ]:
+
+
+7.4
+Creating list thing with given elements as follows 
+thing=[mozzarella", "cinderella ", "salmonella "]
+7.5
+Capitalize the element with a name and print the lis
+
+thing[1]=thing[1].capitalize()
+print(thing)
+ 7.6
+Making the cheesy element all in uppercase 
+thing[0]=thing[0].upper()
+print(thing)
+7.7
+Deleting disease from thing 
+
+thing.remove(thing[2])
+print(thing)
+       
+       
+       Add earning a noble prize as a new element in the achievements list"
+       
+thing.append("Noble Prize")
+ print(thing)
+
+
+# In[ ]:
+
+
+
+
